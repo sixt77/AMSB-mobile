daaae14de0848c18b51d6c1f3c19ab1d43ecc59b
sacha <sacha.favre-teylaz@etu.univ-smb.fr>
2019-02-25 18:45:40 +0100

    -prise en compte des roles 1/X

--- asmb/www/js/api.js
+++ asmb/www/js/api.js
@@ -1,10 +1,17 @@
 function login(identifiant, password){
     var request = new XMLHttpRequest();
-    request.open('GET', 'http://os-vps418.infomaniak.ch/etu_info/amsb1/DEV/index.php/api?login='+identifiant+'&password='+password, false);  // `false` makes the request synchronous
+    request.open('GET', 'http://os-vps418.infomaniak.ch/etu_info/amsb1/DEV/index.php/api?action=login&login='+identifiant+'&password='+password, false);  // `false` makes the request synchronous
     request.send(null);
     if (request.status === 200) {
         return JSON.parse(request.responseText);
     }
-
 }
 
+function get_user_info(user_id){
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://os-vps418.infomaniak.ch/etu_info/amsb1/DEV/index.php/api?action=get_user_info&user_id='+user_id, false);  // `false` makes the request synchronous
+    request.send(null);
+    if (request.status === 200) {
+        return JSON.parse(request.responseText);
+    }
+}
--- asmb/www/js/controller.js
+++ asmb/www/js/controller.js
@@ -1,8 +1,9 @@
-function login(log){
-    var identifiant = document.log.mail.value;
-    var password = document.log.pass.value;
-    var user_info = login(identifiant, password);
+function connect(log){
+    identifiant = document.log.mail.value;
+    password = document.log.pass.value;
+    user_info = login(identifiant, password);
     if(user_info.user_id != undefined){
+        console.log(user_log = (get_user_info(user_info.user_id)));
         document.getElementById('connection').style.visibility ='hidden';
         document.getElementById('homeText').innerText = 'bonjour '+user_info.user_first_name;
         document.getElementById('accueil').style.visibility = 'visible';
