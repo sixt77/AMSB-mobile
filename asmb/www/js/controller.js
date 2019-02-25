5875e296f665b13a1825744a116b5a3afeb4684b
sacha <sacha.favre-teylaz@etu.univ-smb.fr>
2019-02-25 18:23:52 +0100

    -ajout déconnection et message de bienvenue

--- asmb/www/js/controller.js
+++ asmb/www/js/controller.js
@@ -1,6 +1,18 @@
-function connect(log){
+function login(log){
     var identifiant = document.log.mail.value;
     var password = document.log.pass.value;
     var user_info = login(identifiant, password);
-    alert(user_info.user_id);
+    if(user_info.user_id != undefined){
+        document.getElementById('connection').style.visibility ='hidden';
+        document.getElementById('homeText').innerText = 'bonjour '+user_info.user_first_name;
+        document.getElementById('accueil').style.visibility = 'visible';
+    }else{
+        alert('connection refusé');
+    }
+}
+
+function logOff(){
+    document.getElementById('accueil').style.visibility = 'hidden';
+    document.getElementById('homeText').innerText = '';
+    document.getElementById('connection').style.visibility ='visible';
 }
