5051ceb63d02a1b08ee1d47f2f88075d6c5d7a83
Rastaaslan <damien.lagorce@gmail.com>
2019-02-26 15:39:49 +0100

    commit de l'erreur de boutons

--- asmb/www/js/controller.js
+++ asmb/www/js/controller.js
@@ -2,11 +2,12 @@ function connect(log){
     identifiant = document.log.mail.value;
     password = document.log.pass.value;
     user_info = login(identifiant, password);
-    if(user_info.user_id != undefined){
+    if(user_info.user_id !== undefined){
         console.log(user_log = (get_user_info(user_info.user_id)));
         document.getElementById('connection').style.visibility ='hidden';
         document.getElementById('homeText').innerText = 'bonjour '+user_info.user_first_name;
         document.getElementById('accueil').style.visibility = 'visible';
+       test = addButtonRoles();
     }else{
         alert('connection refusé');
     }
@@ -16,4 +17,9 @@ function logOff(){
     document.getElementById('accueil').style.visibility = 'hidden';
     document.getElementById('homeText').innerText = '';
     document.getElementById('connection').style.visibility ='visible';
+}
+
+function addButtonRoles(){
+    document.getElementById('accueil').innerhtml = "<input type='button'>"
+    return false;
 }
