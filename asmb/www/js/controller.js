27df1ecbf4833b7c5ef6577b35812469519ed758
sacha <sacha.favre-teylaz@etu.univ-smb.fr>
2019-03-01 20:35:39 +0100

    -affichage des matchs

--- asmb/www/js/controller.js
+++ asmb/www/js/controller.js
@@ -2,10 +2,11 @@ function connect(log){
     identifiant = document.log.mail.value;
     password = document.log.pass.value;
     user_info = login(identifiant, password);
+    console.log(user_info);
     if(user_info.user_id !== undefined){
         user_role = (get_user_info(user_info.user_id));
         document.getElementById('connection').style.display ='none';
-        document.getElementById('homeText').innerText = 'bonjour '+user_info.user_first_name;
+        document.getElementById('homeText').innerText = 'bonjour '+user_info.user_prenom;
         document.getElementById('accueil').style.display = 'block';
         document.getElementById('menu').style.display = 'block';
         addButtonRoles(user_role);
@@ -17,8 +18,12 @@ function connect(log){
 function logOff(){
     document.getElementById('accueil').style.display = 'none';
     document.getElementById('homeText').innerText = '';
-    document.getElementById('connection').style.display ='none';
+    document.getElementById('connection').style.display ='block';
     document.getElementById('menu').style.display = 'none';
+    closeNav();
+    hide_class("role_div");
+    document.getElementById('button_list').innerHTML = "";
+
 }
 
 function addButtonRoles(user_role){
@@ -29,9 +34,9 @@ function addButtonRoles(user_role){
             var btn = document.createElement("BUTTON");
             btn.setAttribute("id", "test1");
             btn.setAttribute("class", "testButton");
-            btn.setAttribute("onclick", "choixRole('"+role_list[loop]+"')");
+            btn.setAttribute("onclick", "displayRole('"+role_list[loop]+"')");
             btn.innerHTML = role_list[loop];
-            document.getElementById('buttontest').appendChild(btn);
+            document.getElementById('button_list').appendChild(btn);
             loop++;
         }
     }
@@ -45,7 +50,7 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-function timeConverter(UNIX_timestamp){
+function timestampToTime(UNIX_timestamp){
     var a = new Date(UNIX_timestamp * 1000);
     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
     var year = a.getFullYear();
@@ -57,21 +62,43 @@ function timeConverter(UNIX_timestamp){
     var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
     return time;
 }
-function choixRole(role){
-    console.log(role);
+function displayRole(role){
+    hide_class("role_div");
+    document.getElementById(role).style.display ="block";
     if (role == "joueur"){
         var matchs= get_matchs_by_id(user_role.joueur);
     } else {
         var matchs = get_all_matchs();
     }
-    var division = document.getElementById(role);
-    division.style.display = 'block';
-    division.innerHTML = "<table><thead><tr><th>Date et heure</th><th>Equipe 1</th><th>Equipe 2</th></tr></thead><tbody>";
-    for (var i in matchs) {
-        if(matchs[i] != null){
-            division.innerHTML = division.innerHTML + "<tr><td>"+timeConverter(matchs[i].date)+"</td><td>matchs[i].team[0].nom</td><td>matchs[i].team[1].nom</td></tr>";
-            loop++;
-        }
+    var match_divs = document.getElementsByClassName('match_list');
+    for (var i = 0; i < match_divs.length; i++) {
+        match_divs[i].style.display = "block";
+        match_divs[i].innerHTML = display_match(matchs);
     }
-    division.innerHTML = division.innerHTML + "</tbody></table>";
+}
+
+function hide_class(className) {
+    var items = document.getElementsByClassName(className);
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.display = "none";
+    }
+}
+
+function show_class(className) {
+    var items = document.getElementsByClassName(className);
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.display = "block";
+    }
+}
+
+function display_match(matchs){
+    var retour = "";
+    for (x in matchs) {
+        retour = retour.concat("<div class=''>");
+        retour = retour.concat("<p>date : "+timestampToTime(matchs[x]['match']['date'])+"</p>");
+        retour = retour.concat("<p>equipe 1 : "+matchs[x]['team'][0]['nom']+"</p>");
+        retour = retour.concat("<p>equipe 2 : "+matchs[x]['team'][1]['nom']+"</p>");
+        retour =  retour.concat("</div>");
+    }
+    return retour;
 }
